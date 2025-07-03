c6e353b35d0996aadf89ec9617c9a559892ee773
danieldivincenzo <danieldivincenzo14@gmail.com>
2025-07-03 19:35:50 +0200

    Esercizio 6

---
+++ src/Exercise6.java
@@ -0,0 +1,8 @@
+public class Exercise6 {
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5, 6};
+        for (int i = numbers.length; i > 0; i--) {
+            System.out.println(numbers[i - 1]);
+        }
+    }
+}
